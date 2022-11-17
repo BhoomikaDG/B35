8c226fdc800e794e1a63bee7bcca41938a5b6d34
Windows 10 Pro <Windows 10 Pro@DESKTOP-K4O38B7>
2022-11-17 16:29:33 +0530

    first commit

---
+++ Day-1-ConnectngWithGithub/src/org/tnsindia/githdemo/DemoForGitHubConnction.java
@@ -0,0 +1,12 @@
+package org.tnsindia.githdemo;
+
+
+
+public class DemoForGitHubConnction {
+
+	public static void main(String[] args) {
+	      System.out.println("Welcome to TNS India Foundation");
+
+	}
+
+}
