56e9a8990cd9e384a92fdb2c22e4f302e35c4ff3
Windows 10 Pro <Windows 10 Pro@DESKTOP-K4O38B7>
2022-12-08 15:47:47 +0530

    first commit

---
+++ DAY-12 Arrays/src/org/tnsindia/AyyrayDemo/ArrayinputUserDemo.java
@@ -0,0 +1,32 @@
+package org.tnsindia.AyyrayDemo;
+
+import java.util.Scanner;
+
+public class ArrayinputUserDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the size of the array: ");
+		int size=s.nextInt();
+		//array declaration
+		int arr[]=new int[size];
+		System.out.println("Enter the values in the array: ");
+		
+		//to enter the values in the array use for loop
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println("Enter the "+i+"index:");
+			arr[i]=s.nextInt();
+		}
+		//to print the array elements
+		System.out.print("Array elements are:");
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.printf(arr[i]+" ");
+		}
+
+	}
+
+}
---
+++ DAY-12 Arrays/src/org/tnsindia/AyyrayDemo/Varargs.java
@@ -0,0 +1,24 @@
+package org.tnsindia.AyyrayDemo;
+
+public class Varargs {
+	
+	//String ...s is variable -argument and it contain o-n values
+		public static void print(int a,int b,String ...s)
+		{
+			System.out.println(a+" "+b);
+			//to print var args
+			for(int i=0;i<s.length;i++)
+			{
+				System.out.print(s[i]+"\t");
+			}
+			
+		}
+
+
+	public static void main(String[] args) {
+		//fun call
+				print(12,25,"Java","SpringBoot","HTML","CSS","JS");
+
+	}
+
+}
